48d52d5482d466fe321d2e98ec8d589b142b09fe
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-10-31 15:38:08 +0200

    add ExceptionHandler for validation

--- src/main/java/dev/usenkonastia/api/web/GlobalExceptionHandler.java
+++ src/main/java/dev/usenkonastia/api/web/GlobalExceptionHandler.java
@@ -3,12 +3,22 @@ package dev.usenkonastia.api.web;
 import dev.usenkonastia.api.service.exception.CategoryNotFoundException;
 import dev.usenkonastia.api.service.exception.ProductNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.List;
+
+import static dev.usenkonastia.api.util.PaymentDetailsUtils.getValidationErrorsProblemDetail;
 import static java.net.URI.create;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
@@ -31,5 +41,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         problemDetail.setType(create("category-not-found"));
         problemDetail.setTitle("Category Not Found");
         return problemDetail;
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+                                                                  HttpStatusCode status, WebRequest request) {
+        List<FieldError> errors = ex.getBindingResult().getFieldErrors();
+        List<ParamsViolationDetails> validationResponse =
+                errors.stream().map(err -> ParamsViolationDetails.builder().reason(err.getDefaultMessage()).fieldName(err.getField()).build()).toList();
+        log.info("Input params validation failed");
+        return ResponseEntity.status(BAD_REQUEST).body(getValidationErrorsProblemDetail(validationResponse));
     }
 }
